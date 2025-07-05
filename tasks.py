cf36f87788be1727773a067d470243a0b180bdc4
pr4-kp <saldin@wisc.edu>
2025-07-05 17:38:27 -0400

    tasks update

--- tasks.py
+++ tasks.py
@@ -1,8 +1,8 @@
 from celery import Celery
-# from trace_of_frobenius import a_for_prime
+from trace_of_frobenius import a_for_prime
 
-app = Celery('tasks', broker='amqp://pr4:small2025@137.165.79.192/')
+app = Celery('tasks', broker='amqp://pr4:PASSWORD@137.165.79.192/')
 
 @app.task
 def main_prime(p):
-    return p
+    return a_for_prime(p)
